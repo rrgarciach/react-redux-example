8067cc2ab9bf0ff6c3195de2210cdb27cc171fd7
Ruy Garcia <rrgarciach@gmail.com>
2016-12-12 02:01:54 -0700

    Adjusted template for list items.

--- src/components/RedditSearch.jsx
+++ src/components/RedditSearch.jsx
@@ -35,6 +35,27 @@ export default class RedditSearch extends React.Component {
                             </div>
                         </article>
                     </li>
+                    <li>
+                        <article>
+                            <span className="img"></span>
+                            <h2>Danimal1</h2>
+                            <p>Spring 2014. Never forget.</p>
+                            <div className="actions">
+                                <div className="action">
+                                    <span className="icon comments"></span>
+                                    1693 comments
+                                </div>
+                                <div className="action">
+                                    <span className="icon downloads"></span>
+                                    1693 comments
+                                </div>
+                                <div className="action">
+                                    <span className="icon favorites"></span>
+                                    1693 comments
+                                </div>
+                            </div>
+                        </article>
+                    </li>
                 </ul>
             </main>
         </div>
