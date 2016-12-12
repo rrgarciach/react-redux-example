86996d094cb70a095b53d97f7ee85f5d0751fa8d
Ruy Garcia <rrgarciach@gmail.com>
2016-12-12 00:29:01 -0700

    Added font awesome and project assets.

--- src/components/RedditSearch.jsx
+++ src/components/RedditSearch.jsx
@@ -5,9 +5,28 @@ export default class RedditSearch extends React.Component {
     render() {
         return <div>
             <header>
-                <input type="text"/>
+                <div>
+                    <h2>Reddit</h2>
+                    <form>
+                        <input type="text"/>
+                    </form>
+                </div>
             </header>
-            <main></main>
+            <main>
+                <ul>
+                    <li>
+                        <article>
+                            <span className="img"></span>
+                            <h2>Danimal1</h2>
+                            <p>Spring 2014. Never forget.</p>
+                            <div>
+                                <i className="fa fa-camera-retro fa-lg"></i>
+                                <p>1693 comments</p>
+                            </div>
+                        </article>
+                    </li>
+                </ul>
+            </main>
         </div>
     }
 
